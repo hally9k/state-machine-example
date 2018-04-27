a688b2e599322a3424a537491aa45afd93f90186
hally9k <hally9k@gmail.com>
2018-04-27 14:03:59 +1200

    Removed redundant libs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from 'component'
-import { applyMiddleware, createStore } from 'redux'
-import { createEpicMiddleware } from 'redux-observable'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import { List } from 'immutable'
 
 ReactDOM.render(<App />, document.getElementById('root'))

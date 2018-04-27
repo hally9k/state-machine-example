c7d9a109707fdcf447cdffaf21aac7fc5915dfb0
hally9k <hally9k@gmail.com>
2018-04-27 14:01:59 +1200

    ...

--- src/index.js
+++ src/index.js
@@ -2,22 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from 'component'
 import { applyMiddleware, createStore } from 'redux'
-import reducers, { epics } from 'duck'
 import { createEpicMiddleware } from 'redux-observable'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { List } from 'immutable'
-import { Provider } from 'react-redux'
-import logger from 'redux-logger'
 
-const epicMiddleware = createEpicMiddleware(epics)
-
-const enhancer = composeWithDevTools(applyMiddleware(epicMiddleware, logger))
-
-const store = createStore(reducers, { image: null }, enhancer)
-
-ReactDOM.render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
-	document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
